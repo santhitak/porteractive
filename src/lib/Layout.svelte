7435743611ffa2e91dc4ee92442975b545ef194a
santhitak <santhita.sk@gmail.com>
2022-09-18 18:11:05 +0700

    feat: layout

---
+++ src/lib/Layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Link } from "svelte-routing";
+</script>
+
+<div>
+  <nav class="p-5 flex justify-center">
+    <Link to="/" class="mx-10">Home</Link>
+    <Link to="about" class="mx-10">About</Link>
+    <Link to="blog" class="mx-10">Blog</Link>
+  </nav>
+  <slot />
+</div>
---
+++ src/views/Home.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="p-10">
+  <div class="flex flex-col justify-center align-middle">
+    <h1 class="text-6xl font-sans-500">Santhita</h1>
+    <p class="my-5">
+      Hi , this is my first web portfolio which I decided to call it
+      <i class="font-tilt-500">PorterActive</i>
+    </p>
+  </div>
+</div>
