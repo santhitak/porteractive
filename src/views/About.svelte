14adf87adff8487f36766924ff85a41e265a6331
santhitak <santhita.sk@gmail.com>
2022-09-21 18:56:46 +0700

    feat: about and project layout

---
+++ src/views/About.svelte
@@ -0,0 +1,8 @@
+<div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-24 mb-16 mx-10">
+  <div class="bg-gray-300 h-60">01</div>
+  <div class="bg-gray-500 h-60">09</div>
+  <div class="bg-gray-500 h-60">09</div>
+  <div class="bg-gray-300 h-60">01</div>
+  <div class="bg-gray-300 h-60">01</div>
+  <div class="bg-gray-500 h-60">09</div>
+</div>
---
+++ src/views/Project.svelte
@@ -0,0 +1,5 @@
+<script lang="ts"></script>
+
+<div style="height: 100vh;" class="mt-24 mb-16 mx-10">
+  <div class="h-full bg-gray-200" />
+</div>
