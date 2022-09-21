a39fdd128ff9bbc95a07713f36e031d4fafd77ed
santhitak <santhita.sk@gmail.com>
2022-09-21 19:28:31 +0700

    feat: github and linked in

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 </script>
 
-<div style="height: 90vh;">
-  <div class="flex flex-col justify-center align-middle h-full">
+<div style="height: 100vh;">
+  <div class="flex flex-col justify-center align-middle h-full w-full">
     <div class="text-center">
       <h1 class="lg:text-9xl md:text-8xl text-6xl font-sans-500">Santhita</h1>
+    </div>
+  </div>
+  <div class="absolute bottom-0 left-0 w-full">
+    <hr />
+    <div class="h-32 flex justify-around items-center">
+      <a href="https://github.com/santhitak" alt="" target="_blank">GITHUB</a>
+      <a href="https://www.linkedin.com/in/santhita/" alt="" target="_blank">
+        LINKEDIN
+      </a>
     </div>
   </div>
 </div>
