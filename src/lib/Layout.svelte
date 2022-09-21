2ff652b2a08dca596f8f8aacb44401b11aee136a
santhitak <santhita.sk@gmail.com>
2022-09-21 19:28:18 +0700

    feat: background-blur

--- src/lib/Layout.svelte
+++ src/lib/Layout.svelte
@@ -3,11 +3,14 @@
 </script>
 
 <div style="position: fixed; width: 100vw;">
-  <nav class="p-3 flex justify-between bg-white">
+  <nav
+    class="p-3 flex justify-between backdrop-blur-sm"
+    style="background-color: rgba(255, 255, 255, 0.6)"
+  >
     <Link to="/" class="mx-5 lg:text-lg">INDEX</Link>
     <Link to="project" class="mx-5 lg:text-lg">PROJECTS</Link>
     <Link to="about" class="mx-5 lg:text-lg">ABOUT</Link>
   </nav>
-  <hr />
+  <hr class="bg-current" />
 </div>
 <slot />
