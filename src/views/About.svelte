68a79a5193102cd6a5c4e3036ec38b19e6aa3bbf
santhitak <santhita.sk@gmail.com>
2022-09-22 01:18:19 +0700

    feat: about me layout

--- src/views/About.svelte
+++ src/views/About.svelte
@@ -1,8 +1,73 @@
-<div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-24 mb-16 mx-10">
-  <div class="bg-gray-300 h-60">01</div>
-  <div class="bg-gray-500 h-60">09</div>
-  <div class="bg-gray-500 h-60">09</div>
-  <div class="bg-gray-300 h-60">01</div>
-  <div class="bg-gray-300 h-60">01</div>
-  <div class="bg-gray-500 h-60">09</div>
+<div class="w-full h-full flex justify-center">
+  <table
+    class="lg:table-fixed border-collapse border border-slate-400 mt-24 mb-16 mx-10"
+  >
+    <tbody>
+      <tr>
+        <td
+          class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32 text-9xl text-center"
+          >AB</td
+        >
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32">
+          <img
+            src="https://scontent.fbkk4-2.fna.fbcdn.net/v/t39.30808-6/307291126_2023635124510898_8937853553803793475_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHXjjzDNuLeHEgXzDva0jakhMMAhPlyptWEwwCE-XKm1W8ACCkUzr5-SVXKx-2sOUZeFiWn_4uYE37tzV1du9TJ&_nc_ohc=2UOXUTl5aGkAX9-3pVX&_nc_zt=23&_nc_ht=scontent.fbkk4-2.fna&oh=00_AT-jNYmYnis_2mfSeVQoKyUASaOIsYlv18vUySmqWpTtWg&oe=6330F681"
+            alt=""
+            class="w-3/5 m-auto"
+          />
+        </td>
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32">
+          <p class="m-6">
+            Lorem, ipsum dolor sit amet adipisicing . Molestias tenetur eos vero
+            deserunt, voluptates rem architecto dolorem, possimus magnam
+            corporis minima quam sed quidem odit necessitatibus?
+          </p>
+        </td>
+        <td
+          class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32 text-9xl text-center"
+        >
+          OU
+        </td>
+      </tr>
+      <tr>
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32">
+          <p class="m-6">Voluptates quas rem architecto lorem</p></td
+        >
+        <td
+          class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32 text-9xl text-center"
+        >
+          T
+        </td>
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32">
+          <img src="" alt="" class="w-3/5 m-auto" />
+        </td>
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32">
+          <p class="m-6">
+            Lorem, ipsum dolor sit minima corrupti quam blanditiis sed quidem
+            odit necessitatibus?
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td
+          class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32 text-9xl text-center"
+        >
+          ME
+        </td>
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32" />
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/7786/7786056.png"
+            alt=""
+            class="w-3/5 m-auto"
+          />
+        </td>
+        <td class="border border-slate-300 lg:h-96 lg:w-96 h-32 w-32">
+          <ul class="text-center">
+            <li class="my-2">santhita.sk@gmail.com</li>
+            <li class="my-2">(+66) 62 961 5353</li>
+          </ul>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </div>
